d0a150a6cfcd7d72b86947baf13895c230d652a2
Satertek <34286756+Satertek@users.noreply.github.com>
2017-12-05 16:03:58 -0600

    Update advent5.m

--- advent5.m
+++ advent5.m
@@ -1,8 +1,20 @@
-file=load('advent5_list')
-a=file.advent5_list;
+file=load('advent5_list.mat');
 
+%% Part A %%
+a=file.advent5_list;
+i=1;
+loop_count = 0;
+while i < length(a)+1
+    j = a(i);
+    a(i) = a(i) + 1;
+    i = i + j;
+    loop_count = loop_count + 1; 
+end
+
+loop_count
 
 %% Part B %%
+a=file.advent5_list;
 i=1;
 loop_count = 0;
 while i < length(a)+1
